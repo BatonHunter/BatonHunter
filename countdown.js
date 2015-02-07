f34726f616f3e67d36ef07bb456cf1592b61037a
Jih-shien Lu <jslu0413@gmail.com>
2015-02-07 14:58:51 +0800

    Integrate circular-progress.js

--- countdown.js
+++ countdown.js
@@ -1,10 +1,21 @@
-var CountDown = function(domId, totalTime, timeUpCallback) {
+var CountDown = function(domId, radius, totalTime, timeUpCallback) {
+	var progress = new CircularProgress({
+	  radius: radius,
+	  strokeStyle: 'blue',
+	  lineCap: 'round',
+	  lineWidth: 4
+	});
+
+	$(domId).append(progress.el);
+
+
 	var timer = new Timer({
 		totalTimeSecond: totalTime,
 		callbackTimeUp: timeUpCallback,
 		callbackUpdatePerSecond: function(totalTimeSec, currentTimeSec) {
 		        var p = currentTimeSec*100/totalTimeSec;
-			$(domId).attr("style", "width: " + p + "%").text(currentTimeSec);
+			// $(domId).attr("style", "width: " + p + "%").text(currentTimeSec);
+			progress.update(p, currentTimeSec);
 		    }
 		});
 

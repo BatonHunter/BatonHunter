31b80fb6c2d9c5558a7c581c74e5ad23f86042d5
Cloud <eucylin@gmail.com>
2015-02-07 15:30:09 +0800

    移除this.maxHP

--- Scripts/HP.js
+++ Scripts/HP.js
@@ -1,6 +1,5 @@
-var HP = function(max){
-	this.maxHP = max;
-	this.value = this.maxHP;
+var HP = function(maxHP){
+	this.value = maxHP;
 
 	HP.prototype.getValue = function(){
 		return this.value;
@@ -18,11 +17,18 @@ var HP = function(max){
 				this.value += amount;
 				break;
 			case 2:
-				this.value += (this.maxHP * amount * 0.01);
+				this.value += (maxHP * amount * 0.01);
+				console.log(this);
+				console.log(maxHP);
 				break;
 			default:
 				console.log("TYPE ERROR");
 				break;
 			}
+		if(this.value > maxHP){
+			this.value = maxHP;
+		}else if(this.value < 0){
+			this.value = 0;
+		}
 	}
 }

dd605b3c6eb23f58c2480a60e4c7510062ba3b2f
chiyt27 <hipatricia27@gmail.com>
2015-02-07 15:17:37 +0800

    整合測試一版

--- Scripts/HP.js
+++ Scripts/HP.js
@@ -1,6 +1,6 @@
-var HP = function(max){
-	this.maxHP = max;
-	this.value = this.maxHP;
+var HP = function(maxHP){
+	this.max = maxHP;
+	this.value = maxHP;
 
 	HP.prototype.getValue = function(){
 		return this.value;
@@ -18,11 +18,16 @@ var HP = function(max){
 				this.value += amount;
 				break;
 			case 2:
-				this.value += (this.maxHP * amount * 0.01);
+				this.value += (maxHP * amount * 0.01);
 				break;
 			default:
 				console.log("TYPE ERROR");
 				break;
 			}
+		if(this.value > this.max){
+			this.value = this.max;
+		}else if(this.value < 0){
+			this.value = 0;
+		}
 	}
 }

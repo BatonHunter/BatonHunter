a3805b03eefe9d3b18f30c081c9f03fa72a3c3c8
Cloud <eucylin@gmail.com>
2015-02-07 14:44:37 +0800

    增加HP最大上限  現在HP不會突破天際

---
+++ HP.js
@@ -0,0 +1,30 @@
+var HP = function(maxHP){
+	this.value = maxHP;
+
+	HP.prototype.getValue = function(){
+		return this.value;
+	}
+	HP.prototype.isDead = function() {
+		if(this.value <= 0){
+			return true;
+		}else{
+			return false;
+		}
+	};
+	HP.prototype.setHP = function(amount, type){
+		switch (type){
+			case 1:
+				this.value += amount;
+				break;
+			case 2:
+				this.value += (maxHP * amount * 0.01);
+				break;
+			default:
+				console.log("TYPE ERROR");
+				break;
+			}
+		if(this.value >= maxHP){
+			this.value = maxHP;
+		}
+	}
+}

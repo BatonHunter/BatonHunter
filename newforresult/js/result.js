e0854b1181d85980fbd7bf8c55bf4621f3d58628
Cloud <eucylin@gmail.com>
2015-02-14 15:31:08 +0800

    delete page2

--- newforresult/js/result.js
+++ newforresult/js/result.js
@@ -1,17 +1,16 @@
 var result = function() {
-	var isWin;
-	var timeCost;
-	var money = ((Math.random() * 500) + 100);
-	var exp = ((Math.random() * 500) + 100);
-	var score = ((Math.random() * 2000) + 1000);
+	var money = (parseInt(Math.random() * 500) + 100);
+	var exp = (parseInt(Math.random() * 500) + 100);
+	var score = (parseInt(Math.random() * 2000) + 1000);
 
 
 	result.prototype.callResultPage = function(isWin, timeCost){
 		if(typeof(Storage) !== "undefined"){
+			//cache[JoshWang]: for total fighting time storage
 			if(isWin){
+				localStorage.timeCost = timeCost;
 				location.href = "win.html"; 
 			}else{
-				//cache[JoshWang]: for total fighting time storage
 				localStorage.timeCost = timeCost;
 				location.href = "lose.html";
 			}

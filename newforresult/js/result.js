0b726e297d4190cec83bcdbbea7e668012002b70
Cloud <eucylin@gmail.com>
2015-02-14 13:37:06 +0800

    加入result.js(未完成)

---
+++ newforresult/js/result.js
@@ -0,0 +1,15 @@
+var result = function() {
+	this.money;
+	this.exp = ((Math.random() * 500) + 100);
+	this.score = ((Math.random() * 2000) + 1000);
+
+	result.prototype.callResultPage = function(isWin, timeCost){
+		this.isWin = isWin;
+		this.timeCost = timeCost;
+		if(isWin){
+			location.href = "win.html"; 
+		}else{
+			location.href = "lose.html";
+		}
+	}
+}

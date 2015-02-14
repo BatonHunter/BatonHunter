c4c7f56cb1e8173305436cab30827e6acb4fa908
MikiSayaka <mikisayaka@mikisayaka-ThinkPad-Edge-E125.(none)>
2015-02-14 15:18:30 +0800

    Add fancybox effect.

--- assets/js/battleui.js
+++ assets/js/battleui.js
@@ -13,4 +13,20 @@ $(document).ready(function (){
   //blood obj init
   enemyHP = new HP(1000, $('#enemy-hp'));
   userHP = new HP(1000, $('#user-hp'));
+  //  click lucky star
+  $('#luckystar').fancybox({
+    hideOnOverlayClick: false,
+    onStart: function(){
+      //  do something before box closed.
+    },
+    onComplete: function(){
+      //  do something before box closed.
+    },
+    onCleanup: function(){
+      //  do something before box closed.
+    },
+    onClosed: function(){
+      //  do something when box closed.
+    }
+  });
 });

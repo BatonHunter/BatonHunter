62211f3cd4db13e0f25361470291f02f8fa7b2f6
Johnson Chou <joujiahe@gmail.com>
2015-03-07 15:44:12 +0800

    Implement lucky star feature.
    Signed-off-by: Johnson Chou <joujiahe@gmail.com>

--- assets/js/battleui.js
+++ assets/js/battleui.js
@@ -7,36 +7,68 @@ var userHP;
 
 $(document).ready(function (){
   //count down clock init
-  batontimer.setUpClock('#counter', 20);
-  batontimer.reset();
-  batontimer.start();
+  var hintTimer = Object.create(batontimer);
+  var battleTimer = Object.create(batontimer);
+
+  battleTimer.setUpClock('#counter', 20, 'red', 'circle', function () {
+    alert('You die!');
+  });
+  hintTimer.setUpClock('#hint-timer', 30, 'red', 'circle', function () {
+    $.fancybox.close();
+  });
+
+  battleTimer.reset();
+  battleTimer.start();
   //blood obj init
   enemyHP = new HP(1000, $('#enemy-hp'));
   userHP = new HP(1000, $('#user-hp'));
-  //  click lucky star
-  var countDown;
-  $('#luckystar').fancybox({
-    hideOnOverlayClick: false,
-    onStart: function(){
-       $("#divtoBlink").show();
 
-        setInterval(function(){
-         $("#fancybox-overlay").removeAttr("style").toggleClass("backgroundRed");
-        },1000);  
+  var countDown,
+      blink,
+      $luckystar = $('#luckystar');
+
+  //  click lucky star
+  $luckystar.fancybox({
+    hideOnOverlayClick: false,
+    onStart: function() {
+      var $overlay = $('#fancybox-overlay');
+      $overlay.removeAttr('style');
+      blink = setInterval(function () {
+        $overlay.toggleClass("background-red");
+        if (hintTimer.currentTimeSecond <= 7) {
+          clearInterval(blink);
+          blink = setInterval(function () {
+            $overlay.toggleClass("background-red");
+            if (hintTimer.currentTimeSecond <= 3) {
+              clearInterval(blink);
+              blink = setInterval(function () {
+                $overlay.toggleClass("background-red");
+              }, 200);
+            }
+          }, 500);
+        }
+      }, 1000);
+
+      hintTimer.reset();
+      hintTimer.start();
+
+      battleTimer.pause();
     },
     onComplete: function(){
       //  do something before box closed.
       var countValue = $('span.countdown').html();
         countDown = setInterval(function() {
-          console.log(countValue);
+          //console.log(countValue);
         }, 100);
     },
     onCleanup: function(){
       //  do something before box closed.
       clearInterval(countDown);
+      clearInterval(blink);
     },
     onClosed: function(){
       //  do something when box closed.
+      battleTimer.start();
     }
   });
 });

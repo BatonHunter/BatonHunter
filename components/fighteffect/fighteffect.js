c128c5cc5fee0d86b6bbb48d170dd47204e3e622
Kobe Yu <kobe0308@gmail.com>
2015-03-07 16:12:29 +0800

    ready to pull

--- components/fighteffect/fighteffect.js
+++ components/fighteffect/fighteffect.js
@@ -1,4 +1,11 @@
 function fighteffect(type, element)
 {
+
+	//alert('123'+type);
+ 
 	$(element).css('background-image', 'url(assets/img/avatar_fight.jpg)');  
-}	
+  
+
+ 	$(element).effect("bounce", "slow" );
+
+}

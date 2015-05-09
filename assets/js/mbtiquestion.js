87330ff7ec99fd7c166e672aae3e3c965aa2e3c2
MikiSayaka <mikisayaka@mikisayaka-ThinkPad-Edge-E125.(none)>
2015-05-09 15:24:31 +0800

    Bug fix about label tag cannot use at input button at Chrome.

--- assets/js/mbtiquestion.js
+++ assets/js/mbtiquestion.js
@@ -3,7 +3,7 @@
 
 mbti_questionloader.start('#mbti_container');
 $(document).ready(function() {
-  var _mbtiButton = $('input.btn');
+  var _mbtiButton = $('span.btn');
   _mbtiButton.click(function(){
     _mbtiButton.switchClass('btn-success','btn-default');
     $(this).switchClass('btn-default','btn-success');

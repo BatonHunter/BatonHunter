982d3bdd7b89caf8ebac4e01cdedcf1d334f6013
TonyKuo <TonyKuo@TonyKuodeMacBook-Pro.local>
2015-05-23 17:10:11 +0800

    complete event complete

--- components/battlepage/trainingRoom/dataloader/dataloader.js
+++ components/battlepage/trainingRoom/dataloader/dataloader.js
@@ -1,55 +1,54 @@
+var DataLoader = (function() {
+
+    function getstatusString(isSuccess) {
+        var result = "";
+        if (isSuccess == null)
+            return result;
+
+        if (isSuccess) {
+            result = "Complete";
+        }
+        return result;
+    }
+
+    function getUserTutorialStatus(tutorialId, user) {
+        var userTutorials = user.getCurrentJob().tutorialStatus;
+
+        for (var i = 0; i < userTutorials.length; i++) {
+            if (tutorialId == userTutorials[i].id) {
+                var isSuccess = userTutorials[i].status;
+                return isSuccess;
+            }
+        }
+        return null;
+    }
 
 
-   var DataLoader = (function(){
-        
-       function getstatusString(isSuccess){
-         var result ="";
-         if(isSuccess == null)
-          return result;
 
-	   if(isSuccess){
-              result="Complete";
-	  }
-	   return result;
-       }	 
-
-        function getUserTutorialStatus(tutorialId,user){
-          var userTutorials = user.getCurrentJob().tutorialStatus;
-
-          for (var i = 0; i < userTutorials.length; i++) {
-          	if (tutorialId == userTutorials[i].id){
-                    var isSuccess = userTutorials[i].status; 
-		    return isSuccess; 
-		}
-          }
-          return null;
-	}	    
-
-	
-
-	return {
-	    loadTutorials:function(dom_id,user){
-                var tutorials = training_datas.getDatas();
-		var tutorialsCount = tutorials.length;
-		var tutorialsHtml = "<ul class='jobsTutorials list-group'>";
-
-		    
-		for (var i=0;i<tutorialsCount;i++){
-                  var tutorialStatus = getUserTutorialStatus(tutorials[i].id,user);
-		  var tutorialStatusString = getstatusString(tutorialStatus);
-		  var tutorialTitle = tutorials[i].title; 
-		     
-                  tutorialsHtml += "<li class='list-group-item' data-id='"+ tutorials[i].id +"'>";
-	          tutorialsHtml += "<span class='badge'>" + tutorialStatusString +"</span>"; 
-		  tutorialsHtml += tutorialTitle;
-		  tutorialsHtml += "</li>";
-		}
-		tutorialsHtml += "</ul>";
-		$( "#"+ dom_id).empty();
-		$("#"+dom_id).append(tutorialsHtml);	        
-	    }
-
-	 };
+    return {
+        loadTutorials: function(dom_id, user) {
+        	
+            var tutorials = training_datas.getDatas();
+            var tutorialsCount = tutorials.length;
+            var tutorialsHtml = "<ul class='jobsTutorials list-group'>";
 
 
-    })();
+            for (var i = 0; i < tutorialsCount; i++) {
+                var tutorialStatus = getUserTutorialStatus(tutorials[i].id, user);
+                var tutorialStatusString = getstatusString(tutorialStatus);
+                var tutorialTitle = tutorials[i].title;
+
+                tutorialsHtml += "<li class='list-group-item' data-id='" + tutorials[i].id + "'>";
+                tutorialsHtml += "<span class='badge'>" + tutorialStatusString + "</span>";
+                tutorialsHtml += tutorialTitle;
+                tutorialsHtml += "</li>";
+            }
+            tutorialsHtml += "</ul>";
+            $("#" + dom_id).empty();
+            $("#" + dom_id).append(tutorialsHtml);
+        }
+
+    };
+
+
+})();
--- components/battlepage/trainingRoom/mainTrainingRoom/trainingRoom.js
+++ components/battlepage/trainingRoom/mainTrainingRoom/trainingRoom.js
@@ -1,29 +1,54 @@
-
-(function(){
+(function() {
     var player = battle_data.getPlayer();
-    DataLoader.loadTutorials("tutorials",player);
+    DataLoader.loadTutorials("tutorials", player);
 
     //dialog effect setting
-    $( "#dialog" ).dialog({
-	autoOpen: false,
-	show: {
-	    effect: "blind",
-	duration: 1000
-	},
-	hide: {
-	    effect: "explode",
-	duration: 1000
-	}
+    $("#dialog").dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        }
     });
 
+    $(document).on("click","#tutorials li",function(){
+    	console.log(player);
+        var tutorialId = $(this).data('id');
+        var tutorial = training_datas.getData(tutorialId);
+        var content = tutorial.content
 
-    $("#tutorials").find("li").on("click",function() {            
-	var tutorialId=$(this).data('id');
-	var tutorial=training_datas.getData(tutorialId);
-	var content=tutorial.content
+        //set player state
+        player.setTutorialState(tutorialId);
 
-	$("#dialog").find('p').html(content);
-	$("#dialog").dialog( "open" );
+        //refresh page
+        DataLoader.loadTutorials("tutorials", player);
+
+
+
+        $("#dialog").find('p').html(content);
+        $("#dialog").dialog("open");
+
     });
+    //  $("#tutorials").find("li").on("click", function() {
+    //     console.log(player);
+    //     var tutorialId = $(this).data('id');
+    //     var tutorial = training_datas.getData(tutorialId);
+    //     var content = tutorial.content
+
+    //     //set player state
+    //     player.setTutorialState(tutorialId);
+
+    //     //refresh page
+    //     DataLoader.loadTutorials("tutorials", player);
+
+
+
+    //     $("#dialog").find('p').html(content);
+    //     $("#dialog").dialog("open");
+    // });
 
 })();

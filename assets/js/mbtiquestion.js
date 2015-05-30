69cc7f09cf6f8df1e39987ab66935f8955e7dea2
Wei-Ming Chen <morejenfreeman@gmail.com>
2015-05-30 10:29:59 +0800

    MBTI description completed.

--- assets/js/mbtiquestion.js
+++ assets/js/mbtiquestion.js
@@ -3,6 +3,10 @@
 
 mbti_questionloader.start('#mbti_container');
 $(document).ready(function() {
+  $('#mbti_description p span').click(function(){
+      $('#modal_mbti_readmore').on('hidden.bs.modal');
+      $('#modal_mbti_readmore').modal();
+  });
   var _mbtiButton = $('span.btn');
   _mbtiButton.click(function(){
     _mbtiButton.switchClass('btn-success','btn-default');

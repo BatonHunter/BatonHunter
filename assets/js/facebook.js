706d8d8ccc95ffa88316a26d2ac3833f6b9f585e
Johnson Chou <joujiahe@gmail.com>
2015-07-11 16:59:17 +0800

    Sketch careers page.
    Signed-off-by: Johnson Chou <joujiahe@gmail.com>

--- assets/js/facebook.js
+++ assets/js/facebook.js
@@ -44,7 +44,7 @@ var Facebook = (function() {
 
   window.fbAsyncInit = function() {
       FB.init({
-        appId      : '170987831570',
+        appId      : '381627358607336',
         cookie     : true,  // enable cookies to allow the server to access
                             // the session
         xfbml      : true,  // parse social plugins on this page

54f582354cca1609e4e6f782c3e461ebc658968f
mikisayaka <a8965128@gmail.com>
2015-07-25 15:38:30 +0800

    Add some title tag, cokkies data function.

--- components/result_page/js/result.js
+++ components/result_page/js/result.js
@@ -1,28 +1,36 @@
 jQuery(function($){
   $(document).ready(function(){
-    var isWin = true;
-    var isLvUp = true;
-    var money = 1000;
-    var exp = 350;
+
     //  TODO  自cookie獲得經驗值,金錢和是否升級資料
-    $("#money").text(money);
-    $("#exp").text(exp);
-    //  DONE  根據是否升級的資料顯示升級的畫面,同時升級畫面中也顯示社群分享按鈕
-    if (isLvUp) {
-    $('#modal_fight_result').modal();
-        new Share(".share-button", {
-          networks: {
-            facebook: {
-              app_id: ""
-            }
-          }
-        });
+    //  var isWin = Cookies.getItem('isWin');
+    var isLvUp = Cookies.getItem('isLvUp');
+    var money = Cookies.getItem('money');
+    var exp = Cookies.getItem('exp');
+
+    $('#money').text(money);
+    $('#exp').text(exp);
+
+    //  TODO  根據是否升級的資料顯示升級的畫面,同時升級畫面中也顯示社群分享按鈕
+    if (Boolean(isLvUp)) {
+      $('#modal_fight_result').modal();
+      new Share('.share-button', { networks: { facebook: { app_id: '' } } });
     }
 
     //  TODO  將畫面傳導到拉霸機畫面
     $('p.btn').click(function(){
       //  FIXME  加上拉霸機的畫面
-      location.href = '';
+      location.href = '/components/battleslot/index.html';
+    });
+
+    //  TODO  將分享連結加上title
+    $('body').delegate('div.share-button', 'click', function(){
+      $(this).find('div.social li').each(function(){
+        if ($(this).attr('data-network') == 'google_plus') {
+          $(this).attr('title', 'G+');
+        } else {
+          $(this).attr('title', $(this).attr('data-network'));
+        }
+      });
     });
   });
 });

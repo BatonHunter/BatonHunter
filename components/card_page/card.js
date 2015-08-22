557a34f9560d81a4018f41b5a2d75351a8b46a81
Casey Chang <b00705008@ntu.edu.tw>
2015-08-22 16:13:25 +0800

    rebuild card

--- components/card_page/card.js
+++ components/card_page/card.js
@@ -1,14 +1,9 @@
-	$().ready(function(){
-        $('[rel="tooltip"]').tooltip();
-        
-    });
-    
-    function rotateCard(btn){
-        var $card = $(btn).closest('.card-container');
-        console.log($card);
-        if($card.hasClass('hover')){
-            $card.removeClass('hover');
-        } else {
-            $card.addClass('hover');
-        }
+function rotateCard(btn){
+    var $card = $(btn).closest('.card-container');
+    console.log($card);
+    if($card.hasClass('hover')){
+        $card.removeClass('hover');
+    } else {
+        $card.addClass('hover');
     }
+}

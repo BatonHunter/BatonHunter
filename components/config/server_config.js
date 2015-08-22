445c7d4891b9564b994be532c7232faff6d97d10
mikisayaka <a8965128@gmail.com>
2015-08-22 16:34:56 +0800

    Add mailing function.

--- components/config/server_config.js
+++ components/config/server_config.js
@@ -3,6 +3,8 @@ ServerConfig = (function() {
     'use strict';
     
     var serverBaseUrl = "https://baton-huner-restful-server.herokuapp.com/users";
+    var serverAskUrl = "https://baton-huner-restful-server.herokuapp.com/emails";
+    //  var serverAskUrl = "http://192.168.0.121:4567/emails";
     var fbGraphAPIUrl = "https://graph.facebook.com/";
 
     var getUrl = function(email) {
@@ -21,10 +23,15 @@ ServerConfig = (function() {
         return fbGraphAPIUrl + fbID + "/picture?type=large"
     }
 
+    var askUrl = function() {
+      return serverAskUrl;
+    }
+
     return {
         getUrl: getUrl,
         postUrl: postUrl,
         modifyStrengthUrl: modifyStrengthUrl,
-        getPictureUrl: pictureUrl
+        getPictureUrl: pictureUrl,
+        askUrl: askUrl
     };
 })();
